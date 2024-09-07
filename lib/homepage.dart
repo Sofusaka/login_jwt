933f655d27e6dac02f5461a36ffbdeeb3406f57a
Sofía princess <sofusaka@gmail.com>
2024-09-07 14:05:20 -0500

    cambios d gpt Kjnsdkjnkj

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -5,7 +5,6 @@ import 'package:login_jwt/login_service.dart';
 import 'package:http/http.dart' as http;
 import 'package:login_jwt/producto_single.dart';
 
-
 class Homepage extends StatefulWidget {
   Homepage({Key? key}) : super(key: key);
 
@@ -14,102 +13,69 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
-
-bool _showFavorites = false;
+  bool _showFavorites = false;
   int _crossAxisCount = 2;
   final LoginService _loginService = Get.find<LoginService>();
   double _aspectRatio = 1.5;
   ViewType _viewType = ViewType.grid;
 
+  // Función genérica para obtener productos
+  Future<List<ProductoSingle>> _fetchProductos(String endpoint) async {
+    try {
+      String apiUrl = '192.168.0.16';
+      String sUrl = "http://$apiUrl:3000/$endpoint";
 
-  Future<Widget> productos_total() async {
-      
-        String apiUrl ='192.168.0.16';
-        String sUrl = "http://$apiUrl:3000/productos";
+      String? sToken = await _loginService.getUserData();
+      Map<String, dynamic> TokenFinal = jsonDecode(sToken!);
+      String? token = TokenFinal['token'];
 
-        String? sToken = await _loginService.getUserData();
-        Map<String, dynamic> TokenFinal = jsonDecode(sToken!);
-        String? token = TokenFinal['token'];
+      print("Token RETORNADO: $token");
 
+      final response = await http.get(
+        Uri.parse(sUrl),
+        headers: {
+          'Content-Type': 'application/json; charset=UTF-8',
+          'Authorization': 'Bearer $token'
+        },
+      );
 
-        print("Token RETORNADO: $token");
-      
-        final oRespuesta = await http.get(
-          Uri.parse(sUrl),
-          headers: {'Content-Type': 'application/json; charset=UTF-8', 'Authorization':'Bearer $token'},
-        );
-        print(oRespuesta.body);
+      print(response.body);
 
+      if (response.statusCode == 200) {
+        List<dynamic> jsonData = jsonDecode(utf8.decode(response.bodyBytes));
+        List<ProductoSingle> productItems = [];
 
-
-         List<dynamic> oJsonDatos = jsonDecode(utf8.decode(oRespuesta.bodyBytes));
-     
-        List<Widget> awItems = [];
-      
-        for (var i = 0; i < oJsonDatos.length; i++) {
-          var item = oJsonDatos[i];
-      
-      
-          awItems.add(ProductoSingle(
-              calificacion: item["CALIFICACION"].toString(),
-              img: item["IMAGEN"].toString(),
-              nombre: item["NOMBRE"].toString(),
-              vendedor: item["VENDEDOR"].toString(),
-              id: item["idPRODUCTO"],
+        for (var item in jsonData) {
+          productItems.add(ProductoSingle(
+            calificacion: item["CALIFICACION"].toString(),
+            img: item["IMAGEN"].toString(),
+            nombre: item["NOMBRE"].toString(),
+            vendedor: item["VENDEDOR"].toString(),
+            id: item["idPRODUCTO"],
           ));
-               
         }
-
-      return GridView.count(
-                  crossAxisCount: _crossAxisCount,
-                  childAspectRatio: _aspectRatio,
-                  children: awItems
-                );
-
+        return productItems;
+      } else {
+        throw Exception('Error al cargar productos');
       }
-
-
-
-  Future<Widget> favoritos_total() async {
-  String apiUrl = '192.168.0.16';
-  String sUrl = "http://$apiUrl:3000/favoritos";
-
-  String? sToken = await _loginService.getUserData();
-  Map<String, dynamic> TokenFinal = jsonDecode(sToken!);
-  String? token = TokenFinal['token'];
-
-  print("Token RETORNADO: $token");
-
-  final oRespuesta = await http.get(
-    Uri.parse(sUrl),
-    headers: {'Content-Type': 'application/json; charset=UTF-8', 'Authorization': 'Bearer $token'},
-  );
-  print(oRespuesta.body);
-
-  List<dynamic> oJsonDatos = jsonDecode(utf8.decode(oRespuesta.bodyBytes));
-  List<Widget> awItems = [];
-
-  for (var i = 0; i < oJsonDatos.length; i++) {
-    var item = oJsonDatos[i];
-
-    awItems.add(ProductoSingle(
-      id: item["idPRODUCTO"],
-      calificacion: item["CALIFICACION"].toString(),
-      img: item["IMAGEN"].toString(),
-      nombre: item["NOMBRE"].toString(),
-      vendedor: item["VENDEDOR"].toString(),
-    ));
+    } catch (e) {
+      print('Error: $e');
+      throw Exception('Error al cargar productos');
+    }
   }
 
-  return GridView.count(
-    crossAxisCount: _crossAxisCount,
-    childAspectRatio: _aspectRatio,
-    children: awItems,
-  );
-}
+  // Función para obtener todos los productos
+  Future<List<ProductoSingle>> productosTotal() async {
+    return await _fetchProductos('productos');
+  }
 
+  // Función para obtener productos favoritos
+  Future<List<ProductoSingle>> favoritosTotal() async {
+    return await _fetchProductos('favoritos');
+  }
 
-Widget build(BuildContext context) {
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
@@ -137,9 +103,7 @@ Widget build(BuildContext context) {
             icon: Icon(_showFavorites ? Icons.favorite : Icons.favorite_border),
             onPressed: () {
               setState(() {
-
-                //operación de inversión de valor. Cambia el valor de _showFavorites de true a false y viceversa.
-                _showFavorites = !_showFavorites; 
+                _showFavorites = !_showFavorites; // Alternar favoritos
               });
             },
           ),
@@ -158,28 +122,37 @@ Widget build(BuildContext context) {
             ),
           ],
         ),
-        child: FutureBuilder<Widget>(
-          future: _showFavorites ? favoritos_total() : productos_total(), // Cambiar entre productos y favoritos
-          builder: (BuildContext context, AsyncSnapshot<Widget> snapshot) {
-            if (snapshot.hasData) {
-              return snapshot.data ?? Container();
+        child: FutureBuilder<List<ProductoSingle>>(
+          future: _showFavorites ? favoritosTotal() : productosTotal(), // Cambiar entre productos y favoritos
+          builder: (BuildContext context, AsyncSnapshot<List<ProductoSingle>> snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  CircularProgressIndicator(),
+                  Text("Cargando Información"),
+                ],
+              );
+            } else if (snapshot.hasError) {
+              return const Center(
+                child: Text("Error al cargar productos"),
+              );
+            } else if (snapshot.hasData && snapshot.data!.isNotEmpty) {
+              return GridView.count(
+                crossAxisCount: _crossAxisCount,
+                childAspectRatio: _aspectRatio,
+                children: snapshot.data!,
+              );
+            } else {
+              return const Center(
+                child: Text("No se encontraron productos"),
+              );
             }
-            return const Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                CircularProgressIndicator(),
-                Text("Cargando Información"),
-              ],
-            );
           },
         ),
       ),
     );
   }
-
-
-   
-
 }
 
 enum ViewType { grid, list }
